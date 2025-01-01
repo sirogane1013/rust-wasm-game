b8b2614410c3aabda45a1f92d101d44fbb3a253c
sirogane1013 <contact@sirogane.me>
2025-01-01 17:41:16 +0900

    Init Project

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require("path");
+const CopyPlugin = require("copy-webpack-plugin");
+const WasmPackPlugin = require("@wasm-tool/wasm-pack-plugin");
+
+const dist = path.resolve(__dirname, "dist");
+
+module.exports = {
+    mode: "production",
+    entry: {
+        index: "./js/index.js"
+    },
+    output: {
+        path: dist,
+        filename: "[name].js"
+    },
+    devServer: {
+        static: {
+            directory: dist
+        }
+    },
+    plugins: [
+        new CopyPlugin({
+            patterns: [
+                {from: path.resolve(__dirname, "static"), to: dist}
+            ]
+        }),
+        new WasmPackPlugin({
+            crateDirectory: __dirname,
+        }),
+    ],
+    experiments: {
+        asyncWebAssembly: true,
+        syncWebAssembly: true
+    }
+};
