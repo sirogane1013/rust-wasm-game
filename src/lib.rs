cb8627c190239735a9ece116e7350e839298bf49
sirogane1013 <contact@sirogane.me>
2025-01-01 23:48:15 +0900

    Triangle

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,5 @@
 use wasm_bindgen::prelude::*;
-use web_sys::console;
-
+use wasm_bindgen::JsCast;
 
 // When the `wee_alloc` feature is enabled, this uses `wee_alloc` as the global
 // allocator.
@@ -10,14 +9,32 @@ use web_sys::console;
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
-
 // This is like the `main` function, except for JavaScript.
 #[wasm_bindgen(start)]
 pub fn main_js() -> Result<(), JsValue> {
     console_error_panic_hook::set_once();
 
     // Your code goes here!
-    console::log_1(&JsValue::from_str("Hello world!"));
+    let window = web_sys::window().unwrap();
+    let document = window.document().unwrap();
+    let canvas = document
+        .get_element_by_id("canvas")
+        .expect("Failed to get #canvas element")
+        .dyn_into::<web_sys::HtmlCanvasElement>()?;
+
+    let context = canvas
+        .get_context("2d")?
+        .unwrap()
+        .dyn_into::<web_sys::CanvasRenderingContext2d>()?;
+
+    context.move_to(300.0, 0.0);
+    context.begin_path();
+    context.line_to(0.0, 600.0);
+    context.line_to(600.0, 600.0);
+    context.line_to(300.0, 0.0);
+    context.close_path();
+    context.stroke();
+    context.fill();
 
     Ok(())
 }
