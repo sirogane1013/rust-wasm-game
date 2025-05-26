f7ee07f0dba8a50b9f334da93888f2d5a6d5bfe1
sirogane1013 <contact@sirogane.me>
2025-05-26 21:52:53 +0900

    Add platform_and_stone segment

--- src/game.rs
+++ src/game.rs
@@ -1,9 +1,10 @@
 use crate::engine::{Cell, Game, Image, KeyState, Point, Rect, Renderer, Sheet, SpriteSheet};
 use crate::game::red_hat_boy_states::*;
-use crate::segment::stone_and_platform;
+use crate::segment::{platform_and_stone, stone_and_platform};
 use crate::{browser, engine};
 use anyhow::{anyhow, Result};
 use async_trait::async_trait;
+use rand::prelude::*;
 use serde::Deserialize;
 use std::rc::Rc;
 use web_sys::HtmlImageElement;
@@ -31,6 +32,27 @@ pub struct Walk {
 impl Walk {
     fn velocity(&self) -> i16 {
         -self.boy.walk_speed()
+    }
+
+    fn generate_next_segment(&mut self) {
+        let mut rng = thread_rng();
+        let next_segment = rng.gen_range(0..2);
+
+        let mut next_obstacles = match next_segment {
+            0 => stone_and_platform(
+                self.stone.clone(),
+                self.obstacle_sheet.clone(),
+                self.timeline + OBSTACLE_BUFFER,
+            ),
+            1 => platform_and_stone(
+                self.stone.clone(),
+                self.obstacle_sheet.clone(),
+                self.timeline + OBSTACLE_BUFFER,
+            ),
+            _ => vec![],
+        };
+        self.timeline = rightmost(&next_obstacles);
+        self.obstacles.append(&mut next_obstacles);
     }
 }
 
@@ -812,13 +834,7 @@ impl Game for WalkTheDog {
             });
 
             if walk.timeline < TIMELINE_MINIMUM {
-                let mut next_obstacles = stone_and_platform(
-                    walk.stone.clone(),
-                    walk.obstacle_sheet.clone(),
-                    walk.timeline + OBSTACLE_BUFFER,
-                );
-                walk.timeline = rightmost(&next_obstacles);
-                walk.obstacles.append(&mut next_obstacles);
+                walk.generate_next_segment()
             } else {
                 walk.timeline += velocity;
             }
--- src/segment.rs
+++ src/segment.rs
@@ -10,10 +10,11 @@ const HIGH_PLATFORM: i16 = 375;
 const FLOATING_PLATFORM_SPRITES: &[&str] = &["13.png", "14.png", "15.png"];
 const FLOATING_PLATFORM_BOUNDING_BOXES: &[Rect] = &[
     Rect::new_from_x_y(0, 0, 60, 54),
-    Rect::new_from_x_y(60, 0 , 384 - (60 * 2), 93),
-    Rect::new_from_x_y(384 - 60, 0, 60, 54)
+    Rect::new_from_x_y(60, 0, 384 - (60 * 2), 93),
+    Rect::new_from_x_y(384 - 60, 0, 60, 54),
 ];
 
+//noinspection DuplicatedCode
 pub fn stone_and_platform(
     stone: HtmlImageElement,
     sprite_sheet: Rc<SpriteSheet>,
@@ -33,6 +34,31 @@ pub fn stone_and_platform(
             Point {
                 x: offset_x + FIRST_PLATFORM,
                 y: LOW_PLATFORM,
+            },
+        )),
+    ]
+}
+
+//noinspection DuplicatedCode
+pub fn platform_and_stone(
+    stone: HtmlImageElement,
+    sprite_sheet: Rc<SpriteSheet>,
+    offset_x: i16,
+) -> Vec<Box<dyn Obstacle>> {
+    const INITIAL_STONE_OFFSET: i16 = 150;
+    vec![
+        Box::new(Barrier::new(Image::new(
+            stone,
+            Point {
+                x: offset_x + FIRST_PLATFORM,
+                y: STONE_ON_GROUND,
+            },
+        ))),
+        Box::new(create_floating_platform(
+            sprite_sheet,
+            Point {
+                x: offset_x + INITIAL_STONE_OFFSET,
+                y: HIGH_PLATFORM,
             },
         )),
     ]
