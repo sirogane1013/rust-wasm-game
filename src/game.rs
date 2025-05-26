e1fb600513781a94cd75074b706f70fdd54dbed9
sirogane1013 <contact@sirogane.me>
2025-05-26 21:44:23 +0900

    Add timeline to Walk

--- src/game.rs
+++ src/game.rs
@@ -10,6 +10,10 @@ use web_sys::HtmlImageElement;
 
 const HEIGHT: i16 = 600;
 
+const TIMELINE_MINIMUM: i16 = 1000;
+
+const OBSTACLE_BUFFER: i16 = 20;
+
 pub enum WalkTheDog {
     Loading,
     Loaded(Walk),
@@ -20,6 +24,8 @@ pub struct Walk {
     boy: RedHatBoy,
     backgrounds: [Image; 2],
     obstacles: Vec<Box<dyn Obstacle>>,
+    stone: HtmlImageElement,
+    timeline: i16,
 }
 
 impl Walk {
@@ -744,6 +750,10 @@ impl Game for WalkTheDog {
                 ));
 
                 let background_width = background.width() as i16;
+
+                let starting_obstacles = stone_and_platform(stone.clone(), sprite_sheet.clone(), 0);
+                let timeline = rightmost(&starting_obstacles);
+
                 Ok(Box::new(WalkTheDog::Loaded(Walk {
                     boy: rhb,
                     backgrounds: [
@@ -756,8 +766,10 @@ impl Game for WalkTheDog {
                             },
                         ),
                     ],
-                    obstacles: stone_and_platform(stone, sprite_sheet.clone(), 0),
+                    obstacles: starting_obstacles,
                     obstacle_sheet: sprite_sheet,
+                    stone,
+                    timeline,
                 })))
             }
             WalkTheDog::Loaded(_) => Err(anyhow!("Error: Game is already initialized!")),
@@ -797,7 +809,19 @@ impl Game for WalkTheDog {
             walk.obstacles.iter_mut().for_each(|obstacle| {
                 obstacle.move_horizontally(velocity);
                 obstacle.check_intersection(&mut walk.boy)
-            })
+            });
+
+            if walk.timeline < TIMELINE_MINIMUM {
+                let mut next_obstacles = stone_and_platform(
+                    walk.stone.clone(),
+                    walk.obstacle_sheet.clone(),
+                    walk.timeline + OBSTACLE_BUFFER,
+                );
+                walk.timeline = rightmost(&next_obstacles);
+                walk.obstacles.append(&mut next_obstacles);
+            } else {
+                walk.timeline += velocity;
+            }
         }
     }
 
@@ -818,4 +842,12 @@ impl Game for WalkTheDog {
             });
         }
     }
+}
+
+fn rightmost(obstacle_list: &Vec<Box<dyn Obstacle>>) -> i16 {
+    obstacle_list
+        .iter()
+        .map(|obstacle| obstacle.right())
+        .max_by(|x, y| x.cmp(&y))
+        .unwrap_or(0)
 }
